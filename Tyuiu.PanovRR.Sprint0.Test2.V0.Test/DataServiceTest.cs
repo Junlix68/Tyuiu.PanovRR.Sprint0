67291380f66cc72e5a37cc8ff3a14ac9e9763cf5
Junli <Junli@DESKTOP-4LALHTE>
2026-09-11 21:07:39 +0500

    Add Task2

---
+++ Tyuiu.PanovRR.Sprint0.Test2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PanovRR.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PanovRR.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Роман";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Роман", res);
+        }
+    }
+}
