05c0a21de8c53632e15e3b8732f0bab93fbb6a1f
Junli <Junli@DESKTOP-4LALHTE>
2026-09-12 00:52:11 +0500

    Task3

---
+++ Tyuiu.PanovRR.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PanovRR.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.PanovRR.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
