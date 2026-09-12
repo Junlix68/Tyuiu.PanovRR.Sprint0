5dba3dd3b099cf17575ff37abb87f9e5b44a2ba7
Junli <Junli@DESKTOP-4LALHTE>
2026-09-12 18:50:03 +0500

    Task5

---
+++ Tyuiu.PanovRR.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PanovRR.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.PanovRR.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChecetAddtionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void ChecetSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void ChecetAMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void ChecetDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.PanovRR.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PanovRR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PanovRR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c96e639a-5427-4e95-911a-74fd7a695630")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
